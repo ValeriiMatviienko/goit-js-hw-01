21653a09ff129347936b5584cb4ba7c6e0455909
ValeriiMatviienko <unanimous276@gmail.com>
2020-09-19 14:05:38 +0300

    решает задачу 4

--- js/task-4.js
+++ js/task-4.js
@@ -5,6 +5,19 @@ const pricePerDroid = 3000;
 const CANCELED_BY_USER = 'Отменено пользователем!';
 const ACCESS_DENIED = 'Недостаточно средств на счету!';
 
-let totalPrice; // Write code on this line
-let balanceCredit; // Write code on this line
+let totalPrice = pricePerDroid * orderPieces;
+let balanceCredit = credits - totalPrice;
 let message;
+
+// totalPrice = pricePerDroid * orderPieces;
+// balanceCredit = credits - totalPrice;
+
+if (orderPieces === null) {
+  message = CANCELED_BY_USER;
+} else if (orderPieces <= 7) {
+  message = `Вы купили ${orderPieces} дроидов, на счету осталось ${balanceCredit} кредитов`;
+} else if (orderPieces >= 7) {
+  message = ACCESS_DENIED;
+}
+
+console.log(message);
