32189f3f9190f40c93c110688b91e11eec1ba71e
ValeriiMatviienko <unanimous276@gmail.com>
2020-09-19 13:27:26 +0300

    решает задачу 3

---
+++ js/task-4.js
@@ -0,0 +1,10 @@
+const orderPieces = 4;
+
+const credits = 23580;
+const pricePerDroid = 3000;
+const CANCELED_BY_USER = 'Отменено пользователем!';
+const ACCESS_DENIED = 'Недостаточно средств на счету!';
+
+let totalPrice; // Write code on this line
+let balanceCredit; // Write code on this line
+let message;
